a0e9e1ecde86f673dec2d6c73b48d72b6a89a148
Emilio237 <114707455+Emilio237@users.noreply.github.com>
2022-09-29 18:37:27 -0500

    Add files via upload

---
+++ Sketch.js
@@ -0,0 +1,5 @@
+console.log("Este seria mi primer proyecto en visual studio");
+console.log("Hola usuario");
+console.log("Te sientes bien?");
+console.log("no lo creo");
+console.log("me gustan mis clases");
